b8f33b16e54d0b496df4c3c46f8eb691052521fb
Lex Ruesink <lexruesink@live.nl>
2018-10-28 19:28:25 +0100

    Use id defines

--- lib/pio.hpp
+++ lib/pio.hpp
@@ -1,23 +1,23 @@
 #ifndef LLIB_PIO_HPP
 #define LLIB_PIO_HPP
 
-#include <cstdint>
+#include "sam.h"
 
 namespace llib {
     struct pioa {
-        constexpr static uint32_t instance_id = 11;
+        constexpr static uint32_t instance_id = ID_PIOA;
     };
 
     struct piob {
-        constexpr static uint32_t instance_id = 12;
+        constexpr static uint32_t instance_id = ID_PIOB;
     };
 
     struct pioc {
-        constexpr static uint32_t instance_id = 13;
+        constexpr static uint32_t instance_id = ID_PIOC;
     };
 
     struct piod {
-        constexpr static uint32_t instance_id = 14;
+        constexpr static uint32_t instance_id = ID_PIOD;
     };
 }
 
