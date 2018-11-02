63b8796f9dfe677a9775baa4d801a7ca6cb9fcc9
Lex Ruesink <lexruesink@live.nl>
2018-11-02 21:52:42 +0100

    Anonymous namespace for impl details

--- lib/bitset.hpp
+++ lib/bitset.hpp
@@ -45,181 +45,181 @@ namespace llib {
         struct bitset_type<max_16> {
             using type = uint16_t;
         };
+
+        template<int Bits, typename InternalType, int Variation>
+        class _bitset {
+        protected:
+            InternalType bits = {};
+
+        public:
+            constexpr bool operator[](const int index) const {
+                return test(index);
+            }
+
+            constexpr int count() const {
+                int result = 0;
+
+                for (int i = 0; i < Bits; i++) {
+                    result += test(i);
+                }
+
+                return result;
+            }
+
+            constexpr int size() const {
+                return Bits;
+            }
+
+            constexpr bool test(const int index) const {
+                return (bits >> index & 0x1) != 0;
+            }
+
+            constexpr bool any() const {
+                for (int i = 0; i < Bits; i++) {
+                    if (test(i)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            constexpr bool none() const {
+                for (int i = 0; i < Bits; i++) {
+                    if (test(i)) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            constexpr bool all() const {
+                for (int i = 0; i < Bits; i++) {
+                    if (!test(i)) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            constexpr void set() {
+                bits = std::numeric_limits<uint8_t>::max();
+            }
+
+            constexpr void set(const int index, const bool value = true) {
+                bits ^= (-value ^ bits) << (1UL << index);
+            }
+
+            constexpr void reset() {
+                bits = 0;
+            }
+
+            constexpr void reset(const int index) {
+                bits &= ~(1UL << index);
+            }
+
+            constexpr void flip() {
+                bits = ~bits;
+            }
+
+            constexpr void flip(const int index) {
+                bits ^= 1UL << index;
+            }
+        };
+
+        template<int Bits>
+        class _bitset<Bits, bitset_type<max_other>, max_other> {
+        protected:
+            constexpr static int ArraySize = Bits / 32 + 1;
+
+            bitset_type<max_other> bits[ArraySize] = {};
+
+        public:
+            constexpr bool operator[](const int index) const {
+                return test(index);
+            }
+
+            constexpr int count() const {
+                int result = 0;
+
+                for (int i = 0; i < Bits; i++) {
+                    result += test(i);
+                }
+
+                return result;
+            }
+
+            constexpr int size() const {
+                return Bits;
+            }
+
+            constexpr bool test(const int index) const {
+                return (bits[index / 32] >> (index % 32)) != 0;
+            }
+
+            constexpr bool any() const {
+                for (int i = 0; i < Bits; i++) {
+                    if (test(i)) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+            constexpr bool none() const {
+                for (int i = 0; i < Bits; i++) {
+                    if (test(i)) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            constexpr bool all() const {
+                for (int i = 0; i < Bits; i++) {
+                    if (!test(i)) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            constexpr void set() {
+                for (int i = 0; i < ArraySize; i++) {
+                    bits[i] = std::numeric_limits<uint32_t>::max();
+                }
+            }
+
+            constexpr void set(const int index, const bool value = true) {
+                bits[index / 32] ^= (-value ^ bits[index / 32]) & (1UL << (index % 32));
+            }
+
+            constexpr void reset() {
+                for (int i = 0; i < ArraySize; i++) {
+                    bits[i] = 0;
+                }
+            }
+
+            constexpr void reset(const int index) {
+                bits[index / 32] &= ~(1UL << (index % 32));
+            }
+
+            constexpr void flip() {
+                for (int i = 0; i < ArraySize; i++) {
+                    bits[i] = ~bits[i];
+                }
+            }
+
+            constexpr void flip(const int index) {
+                bits[index / 32] ^= 1UL << (index % 32);
+            }
+        };
     }
-
-    template<int Bits, typename InternalType, int Variation>
-    class _bitset {
-    protected:
-        InternalType bits = {};
-
-    public:
-        constexpr bool operator[](const int index) const {
-            return test(index);
-        }
-
-        constexpr int count() const {
-            int result = 0;
-
-            for (int i = 0; i < Bits; i++) {
-                result += test(i);
-            }
-
-            return result;
-        }
-
-        constexpr int size() const {
-            return Bits;
-        }
-
-        constexpr bool test(const int index) const {
-            return (bits >> index & 0x1) != 0;
-        }
-
-        constexpr bool any() const {
-            for (int i = 0; i < Bits; i++) {
-                if (test(i)) {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        constexpr bool none() const {
-            for (int i = 0; i < Bits; i++) {
-                if (test(i)) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        constexpr bool all() const {
-            for (int i = 0; i < Bits; i++) {
-                if (!test(i)) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        constexpr void set() {
-            bits = std::numeric_limits<uint8_t>::max();
-        }
-
-        constexpr void set(const int index, const bool value = true) {
-            bits ^= (-value ^ bits) << (1UL << index);
-        }
-
-        constexpr void reset() {
-            bits = 0;
-        }
-
-        constexpr void reset(const int index) {
-            bits &= ~(1UL << index);
-        }
-
-        constexpr void flip() {
-            bits = ~bits;
-        }
-
-        constexpr void flip(const int index) {
-            bits ^= 1UL << index;
-        }
-    };
-
-    template<int Bits>
-    class _bitset<Bits, bitset_type<max_other>, max_other> {
-    protected:
-        constexpr static int ArraySize = Bits / 32 + 1;
-
-        bitset_type<max_other> bits[ArraySize] = {};
-
-    public:
-        constexpr bool operator[](const int index) const {
-            return test(index);
-        }
-
-        constexpr int count() const {
-            int result = 0;
-
-            for (int i = 0; i < Bits; i++) {
-                result += test(i);
-            }
-
-            return result;
-        }
-
-        constexpr int size() const {
-            return Bits;
-        }
-
-        constexpr bool test(const int index) const {
-            return (bits[index / 32] >> (index % 32)) != 0;
-        }
-
-        constexpr bool any() const {
-            for (int i = 0; i < Bits; i++) {
-                if (test(i)) {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        constexpr bool none() const {
-            for (int i = 0; i < Bits; i++) {
-                if (test(i)) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        constexpr bool all() const {
-            for (int i = 0; i < Bits; i++) {
-                if (!test(i)) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        constexpr void set() {
-            for (int i = 0; i < ArraySize; i++) {
-                bits[i] = std::numeric_limits<uint32_t>::max();
-            }
-        }
-
-        constexpr void set(const int index, const bool value = true) {
-            bits[index / 32] ^= (-value ^ bits[index / 32]) & (1UL << (index % 32));
-        }
-
-        constexpr void reset() {
-            for (int i = 0; i < ArraySize; i++) {
-                bits[i] = 0;
-            }
-        }
-
-        constexpr void reset(const int index) {
-            bits[index / 32] &= ~(1UL << (index % 32));
-        }
-
-        constexpr void flip() {
-            for (int i = 0; i < ArraySize; i++) {
-                bits[i] = ~bits[i];
-            }
-        }
-
-        constexpr void flip(const int index) {
-            bits[index / 32] ^= 1UL << (index % 32);
-        }
-    };
 
     template<int Bits>
     using bitset = _bitset<
