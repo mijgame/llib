eb201394705fecc0afd012aced436758a39f6505
Lex Ruesink <lexruesink@live.nl>
2018-11-02 22:12:17 +0100

    Add systemInit

--- boot/startup.c
+++ boot/startup.c
@@ -1,3 +1,7 @@
+#define register
+
+#include "sam3.h"
+
 int main();
 
 void fatal_error_detected(const char *);
@@ -11,6 +15,47 @@ __attribute__ ((section(".vectors"))) = {
                 (void (*)(void)) &__stack_end,
                 __startup
         };
+
+/* Clock settings (84MHz) */
+#define SYS_BOARD_OSCOUNT   (CKGR_MOR_MOSCXTST(0x8))
+#define SYS_BOARD_PLLAR     (CKGR_PLLAR_ONE | CKGR_PLLAR_MULA(0xdUL) | CKGR_PLLAR_PLLACOUNT(0x3fUL) | CKGR_PLLAR_DIVA(0x1UL))
+#define SYS_BOARD_MCKR (PMC_MCKR_PRES_CLK_2 | PMC_MCKR_CSS_PLLA_CLK)
+
+uint32_t SystemCoreClock;
+
+void SystemInit(void) {
+    /* Set FWS according to SYS_BOARD_MCKR configuration */
+    EFC0->EEFC_FMR = EEFC_FMR_FWS(4);
+    EFC1->EEFC_FMR = EEFC_FMR_FWS(4);
+
+    /* Initialize main oscillator */
+    if (!(PMC->CKGR_MOR & CKGR_MOR_MOSCSEL)) {
+        PMC->CKGR_MOR = CKGR_MOR_KEY_PASSWD | SYS_BOARD_OSCOUNT | CKGR_MOR_MOSCRCEN | CKGR_MOR_MOSCXTEN;
+        while (!(PMC->PMC_SR & PMC_SR_MOSCXTS)) {
+        }
+    }
+
+    /* Switch to 3-20MHz Xtal oscillator */
+    PMC->CKGR_MOR = CKGR_MOR_KEY_PASSWD | SYS_BOARD_OSCOUNT | CKGR_MOR_MOSCRCEN | CKGR_MOR_MOSCXTEN | CKGR_MOR_MOSCSEL;
+
+    while (!(PMC->PMC_SR & PMC_SR_MOSCSELS));
+    PMC->PMC_MCKR = (PMC->PMC_MCKR & ~(uint32_t) PMC_MCKR_CSS_Msk) | PMC_MCKR_CSS_MAIN_CLK;
+    while (!(PMC->PMC_SR & PMC_SR_MCKRDY));
+
+    /* Initialize PLLA */
+    PMC->CKGR_PLLAR = SYS_BOARD_PLLAR;
+    while (!(PMC->PMC_SR & PMC_SR_LOCKA));
+
+    /* Switch to main clock */
+    PMC->PMC_MCKR = (SYS_BOARD_MCKR & ~PMC_MCKR_CSS_Msk) | PMC_MCKR_CSS_MAIN_CLK;
+    while (!(PMC->PMC_SR & PMC_SR_MCKRDY));
+
+    /* Switch to PLLA */
+    PMC->PMC_MCKR = SYS_BOARD_MCKR;
+    while (!(PMC->PMC_SR & PMC_SR_MCKRDY));
+
+    SystemCoreClock = CHIP_FREQ_CPU_MAX;
+}
 
 void __attribute__((noreturn)) __startup() {
     extern unsigned int __data_init_start;
@@ -35,6 +80,8 @@ void __attribute__((noreturn)) __startup() {
     while (d != e) {
         *d++ = *s++;
     }
+
+    SystemInit();
 
     // call main
     (void) main();
