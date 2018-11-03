8ec921177cafcb8df7c465be1bf1311d2fa96edf
Lex Ruesink <lexruesink@live.nl>
2018-11-03 20:05:48 +0100

    Formatting and cmakelists

--- lib/adc.hpp
+++ lib/adc.hpp
@@ -3,59 +3,59 @@
 
 #include "peripheral.hpp"
 
-namespace llib{
+namespace llib {
 
-    template<typename Pin, bool freerunning = true>
-    class pin_adc{
-        public:
-            constexpr static uint32_t instance_id = ID_ADC;
+    template<typename Pin, bool Freerunning = true>
+    class pin_adc {
+    public:
+        constexpr static uint32_t instance_id = ID_ADC;
 
-            using pin = Pin;
+        using pin = Pin;
 
-            // true set adc to 12 bit, false to 10 bit
-            template<bool low_high_res = false>
-            void init(){
-                // enable adc clock
-                enable_clock<pin_adc>();
-                
-                // Remove pin from pio
-                ADC->ADC_CHER = pins::channel<Pin>;
+        // true set adc to 12 bit, false to 10 bit
+        template<bool LowHighRes = false>
+        static void init() {
+            // enable adc clock
+            enable_clock<pin_adc>();
 
-                // select resolution (10 - 12 bits)
-                ADC->ADC_MR = !low_high_res << 4;
+            // Remove pin from pio
+            ADC->ADC_CHER = pins::channel<Pin>;
 
-                if constexpr (freerunning){
-                    // Enable freerun mode
-                    ADC->ADC_MR |= ADC_MR_FREERUN_ON;
-                }
+            // select resolution (10 - 12 bits)
+            ADC->ADC_MR = static_cast<uint32_t>(!LowHighRes << 4);
 
-                // Disable interupts
-                ADC->ADC_IDR = 0xFFFFFFFF;
-
-                // Set gain to 1(sinds ADC->COR diffx = 0)
-                ADC->ADC_CGR = 0;
-                
-                // Set offset to 0
-                ADC->ADC_COR = 0;
-
-                if constexpr (!freerunning){
-                    // enable data ready interupt
-                    ADC->ADC_IER = ADC_IER_DRDY;
-                }
+            if constexpr (Freerunning) {
+                // Enable freerun mode
+                ADC->ADC_MR |= ADC_MR_FREERUN_ON;
             }
 
-            uint16_t get(){
-                if constexpr(!freerunning){
-                    // Start analog to digital conversion
-                    ADC->ADC_CR = ADC_CR_START;
+            // Disable interupts
+            ADC->ADC_IDR = 0xFFFFFFFF;
 
-                    // Wait until conversion is ready
-                    while((ADC->ADC_ISR & ADC_ISR_DRDY) == 0){}
-                }
+            // Set gain to 1 (since ADC->COR diffx = 0)
+            ADC->ADC_CGR = 0;
 
-                // return last 16 bits
-                return ADC->ADC_CDR[Pin::channel] & 0xFFFF;
+            // Set offset to 0
+            ADC->ADC_COR = 0;
+
+            if constexpr (!Freerunning) {
+                // Enable data ready interrupt
+                ADC->ADC_IER = ADC_IER_DRDY;
             }
+        }
+
+        static uint16_t get() {
+            if constexpr(!Freerunning) {
+                // Start analog to digital conversion
+                ADC->ADC_CR = ADC_CR_START;
+
+                // Wait until conversion is ready
+                while ((ADC->ADC_ISR & ADC_ISR_DRDY) == 0);
+            }
+
+            // Return last 16 bits
+            return ADC->ADC_CDR[Pin::channel] & 0xFFFF;
+        }
 
     };
 }
