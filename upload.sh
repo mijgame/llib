12df8b84f702ea68982f2578378a12aba9d52ad1
Lex Ruesink <lexruesink@live.nl>
2018-11-04 16:35:00 +0100

    Add lpc21isp to the upload script

--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 #stty -F /dev/ttyACM0 1200
 #sleep 2
-bossac -e -w -v -b cmake-build-run/llib.bin -p ttyACM0 -U false -R
+bossac -e -w -v -b cmake-build-llib/llib.bin -p ttyACM0 -U false -R
+./tools/lpc21isp_197/lpc21isp_hr_linux -termonly /dev/ttyACM0 115200 12000
