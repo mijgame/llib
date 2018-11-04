09c368a2b0291a83456ba1bdd9ea1847fdf6516b
Lex Ruesink <lexruesink@live.nl>
2018-11-04 16:43:01 +0100

    Formatting

--- lib/watchdog.hpp
+++ lib/watchdog.hpp
@@ -1,41 +1,43 @@
 #ifndef LLIB_WATCHDOG_HPP
 #define LLIB_WATCHDOG_HPP
 
-#include "peripheral.hpp"
+#include "base.hpp"
 
-namespace llib{
-    namespace watchdog{
-        static void disable(){
-            // stop the watchdog timer
+namespace llib {
+
+    class watchdog {
+    public:
+        static void disable() {
+            // Disable the watchdog timer
             WDT->WDT_MR = WDT_MR_WDDIS;
         }
 
-        static void reload(){
-            // restart the watchdog timer
+        static void restart() {
+            // Restart the watchdog timer
             WDT->WDT_CR = WDT_CR_WDRSTT | WDT_CR_KEY_PASSWD;
         }
 
-        template<unsigned int CounterStart = 0xFFF, unsigned int Delta = 0xFFF, bool reset = true>
-        static void init(){
-            WDT->WDT_MR = WDT_MR_WDIDLEHLT                          // stop watchdog if cpu is in idle mode
-                          | WDT_MR_WDDBGHLT                         // stop watchdog if cpu is in debug state
-                          | WDT_MR_WDFIEN                           // enable watchdog interupts
-                          | WDT_MR_WDV(CounterStart)                // set counter value(default takes 16 seconds to trigger reset)
-                          | WDT_MR_WDD(Delta)                       // set the range the watchdog is allowed to restart
-                                                                    // if restart is before this value it will trow an error
-                          | static_cast<uint32_t>(reset << 13);     // if true this will reset the cpu if underflow or an error occured                        
+        template<unsigned int CounterStart = 0xFFF, unsigned int Delta = 0xFFF, bool Reset = true>
+        static void init() {
+            WDT->WDT_MR = WDT_MR_WDIDLEHLT                          // Stop watchdog if cpu is in idle mode
+                          | WDT_MR_WDDBGHLT                         // Stop watchdog if cpu is in debug state
+                          | WDT_MR_WDFIEN                           // Enable watchdog interrupts
+                          | WDT_MR_WDV(CounterStart)                // Set counter value (default takes 16 seconds to trigger Reset)
+                          | WDT_MR_WDD(Delta)                       // Set the range the watchdog is allowed to restart
+                          // If restart is before this value it will throw an error
+                          | static_cast<uint32_t>(Reset << 13);     // If true this will Reset the cpu if underflow or an error occurred
         }
 
-        static bool watchdog_error(){
-            // returns if an error occured sinds last read
-            return WDT->WDT_SR & WDT_SR_WDERR;
+        static bool watchdog_error() {
+            // Returns if an error occurred since last read
+            return static_cast<bool>(WDT->WDT_SR & WDT_SR_WDERR);
         }
 
-        static bool watchdog_underflow(){
-            // returns if an underflow occured sinds last read
-            return WDT->WDT_SR & WDT_SR_WDUNF;
+        static bool watchdog_underflow() {
+            // Returns if an underflow occurred since last read
+            return static_cast<bool>(WDT->WDT_SR & WDT_SR_WDUNF);
         }
-    }
+    };
 }
 
 #endif
