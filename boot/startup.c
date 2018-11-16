2a94ac71ec87eb20cb6880e3017ed3f140568579
Lex Ruesink <lexruesink@live.nl>
2018-11-16 18:03:39 +0100

    Due-specific startup code is now in a target-specific __target_startup() function. The boot folder will probably be reworked when a new platform (non-cortex) is implemented.

--- boot/startup.c
+++ boot/startup.c
@@ -1,25 +1,20 @@
 #define register
 
-#include "sam3.h"
-
 int main();
 
 void fatal_error_detected(const char *);
 
 void __startup();
 
+
 extern unsigned __stack_end();
+extern void __target_startup();
 
 void (*const volatile __vectors[8])(void)
 __attribute__ ((section(".vectors"))) = {
                 (void (*)(void)) &__stack_end,
                 __startup
         };
-
-/* Clock settings (84MHz) */
-#define SYS_BOARD_OSCOUNT   (CKGR_MOR_MOSCXTST(0x8))
-#define SYS_BOARD_PLLAR     (CKGR_PLLAR_ONE | CKGR_PLLAR_MULA(0xdUL) | CKGR_PLLAR_PLLACOUNT(0x3fUL) | CKGR_PLLAR_DIVA(0x1UL))
-#define SYS_BOARD_MCKR (PMC_MCKR_PRES_CLK_2 | PMC_MCKR_CSS_PLLA_CLK)
 
 void __attribute__((noreturn)) __startup() {
     extern unsigned int __data_init_start;
@@ -45,35 +40,7 @@ void __attribute__((noreturn)) __startup() {
         *d++ = *s++;
     }
 
-    /* Set FWS according to SYS_BOARD_MCKR configuration */
-    EFC0->EEFC_FMR = EEFC_FMR_FWS(4);
-    EFC1->EEFC_FMR = EEFC_FMR_FWS(4);
-
-    /* Initialize main oscillator */
-    if (!(PMC->CKGR_MOR & CKGR_MOR_MOSCSEL)) {
-        PMC->CKGR_MOR = CKGR_MOR_KEY_PASSWD | SYS_BOARD_OSCOUNT | CKGR_MOR_MOSCRCEN | CKGR_MOR_MOSCXTEN;
-        while (!(PMC->PMC_SR & PMC_SR_MOSCXTS)) {
-        }
-    }
-
-    /* Switch to 3-20MHz Xtal oscillator */
-    PMC->CKGR_MOR = CKGR_MOR_KEY_PASSWD | SYS_BOARD_OSCOUNT | CKGR_MOR_MOSCRCEN | CKGR_MOR_MOSCXTEN | CKGR_MOR_MOSCSEL;
-
-    while (!(PMC->PMC_SR & PMC_SR_MOSCSELS));
-    PMC->PMC_MCKR = (PMC->PMC_MCKR & ~(uint32_t) PMC_MCKR_CSS_Msk) | PMC_MCKR_CSS_MAIN_CLK;
-    while (!(PMC->PMC_SR & PMC_SR_MCKRDY));
-
-    /* Initialize PLLA */
-    PMC->CKGR_PLLAR = SYS_BOARD_PLLAR;
-    while (!(PMC->PMC_SR & PMC_SR_LOCKA));
-
-    /* Switch to main clock */
-    PMC->PMC_MCKR = (SYS_BOARD_MCKR & ~PMC_MCKR_CSS_Msk) | PMC_MCKR_CSS_MAIN_CLK;
-    while (!(PMC->PMC_SR & PMC_SR_MCKRDY));
-
-    /* Switch to PLLA */
-    PMC->PMC_MCKR = SYS_BOARD_MCKR;
-    while (!(PMC->PMC_SR & PMC_SR_MCKRDY));
+    __target_startup();
 
     // call main
     (void) main();
