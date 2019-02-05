f543b46daa01bc3dda5e8f88c48cc5112a2ff5bd
Lex Ruesink <lexruesink@live.nl>
2019-02-05 22:46:34 +0100

    Pave the way for proper interrupt handling

--- boot/startup.c
+++ boot/startup.c
@@ -1,12 +1,24 @@
 #include <limits.h>
 #define register
 
+/*
+ * We gave 'weak' attribute, so these functions can be aliased into a single
+ * function
+ */
+void __attribute__((weak)) __reset_handler();     /* Reset Handler */
+void __attribute__((weak)) __nmi_handler();       /* NMI Handler */
+void __attribute__((weak)) __hardfault_handler(); /* Hard Fault Handler */
+void __attribute__((weak)) __memmanage_handler(); /* MPU Fault Handler */
+void __attribute__((weak)) __busfault_handler();  /* Bus Fault Handler */
+void __attribute__((weak)) __usagefault_handler();/* Usage Fault Handler */
+void __attribute__((weak)) __svc_handler();       /* SVCall Handler */
+void __attribute__((weak)) __debugmon_handler();  /* Debug Monitor Handler */
+void __attribute__((weak)) __pendsv_handler();    /* PendSV Handler */
+void __attribute__((weak)) __systick_handler();   /* SysTick Handler */
+
 int main();
 
 void fatal_error_detected(const char *);
-
-void __startup();
-
 
 extern unsigned __stack_end();
 extern void __target_startup();
@@ -14,13 +26,42 @@ extern void __target_startup();
 extern void *memcpy(void *destination, const void *source, size_t num);
 extern void *memset(void *ptr, int value, size_t num);
 
-void (*const volatile __vectors[8])(void)
+void (*const volatile __vectors[])(void)
 __attribute__ ((section(".vectors"))) = {
                 (void (*)(void)) &__stack_end,
-                __startup
+                __reset_handler,      // The reset handler
+                __nmi_handler,        // The NMI handler
+                __hardfault_handler,  // The hard fault handler
+                __memmanage_handler,  // The MPU fault handler
+                __busfault_handler,   // The bus fault handler
+                __usagefault_handler, // The usage fault handler
+                0,                  // Reserved
+                0,                  // Reserved
+                0,                  // Reserved
+                0,                  // Reserved
+                __svc_handler,        // SVCall handler
+                __debugmon_handler,   // Debug monitor handler
+                0,                  // Reserved
+                __pendsv_handler,     // The PendSV handler
+                __systick_handler,    // The SysTick handler
         };
 
-void __attribute__((noreturn)) __startup() {
+void __default_handler(void);
+#pragma weak __nmi_handler        = __default_handler /* NMI handler */
+#pragma weak HardFault_Handler  = __default_handler /* Hard Fault handler */
+#pragma weak MemManage_Handler  = __default_handler /* MPU Fault Handler */
+#pragma weak BusFault_Handler   = __default_handler /* Bus Fault Handler */
+#pragma weak UsageFault_Handler = __default_handler /* Usage Fault Handler */
+#pragma weak SVC_Handler        = __default_handler /* SVCall Handler */
+#pragma weak DebugMon_Handler   = __default_handler /* Debug Monitor Handler */
+#pragma weak PendSV_Handler     = __default_handler /* PendSV Handler */
+#pragma weak SysTick_Handler    = __default_handler /* SysTick Handler */
+
+void __default_handler(void) {
+    while (1);
+}
+
+void __reset_handler() {
     extern unsigned int __data_init_start;
     extern unsigned int __data_start;
     extern unsigned int __data_end;
