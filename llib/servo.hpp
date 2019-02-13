7542662d750801c17cfdc5a62c6fdd39d98458a8
Lex Ruesink <lexruesink@live.nl>
2019-02-13 21:35:37 +0100

    Cleanup

--- llib/servo.hpp
+++ llib/servo.hpp
@@ -5,38 +5,35 @@
 #include <pwm.hpp>
 
 namespace llib {
-    namespace detail {
-        template<uint16_t Pos, uint16_t Hz, uint16_t MinPulse, uint16_t MaxPulse>
-        constexpr uint16_t _dutycycle = (((17 * Hz) * (Pos * (MaxPulse - MinPulse) + 180 * MinPulse)) / 12000000);
-    }
-
-    template<typename Pin, uint16_t Min_pulse = 500, uint16_t Max_pulse = 2500, uint16_t Hz = 100>
+    template<typename Pin, uint16_t MinPulse = 500, uint16_t MaxPulse = 2500, uint16_t Hz = 100>
     class servo {
-        private:        
-            static uint8_t _to_dutycyle(uint32_t Pos){
-                // old method but division is slower
-                // (Pos * (Max_pulse - Min_pulse) / 180 + Min_pulse) / (((1'000'000 / Hz) / 255));
+    protected:
+        constexpr static LLIB_FORCE_INLINE uint8_t _to_dutycyle(const uint32_t Pos) {
+            // old method but division is slower
+            // (Pos * (MaxPulse - MinPulse) / 180 + MinPulse) / (((1'000'000 / Hz) / 255));
 
-                // returns an duty cycle corresponding to the position and frequency
-                return ((17 * Hz) * (Pos * (Max_pulse - Min_pulse) + 180 * Min_pulse)) / 12000000;
-            }
+            // returns an duty cycle corresponding to the position and frequency
+            return static_cast<uint8_t>(
+                ((17 * Hz) * (Pos * (MaxPulse - MinPulse) + 180 * MinPulse)) / 12000000
+            );
+        }
 
-        public:
-            using pin = Pin;
+    public:
+        using pin = Pin;
 
-            template<uint8_t Pos = 127>
-            static void init(){
-                llib::target::pin_pwm<pin>::template init<Hz, detail::_dutycycle<Pos, Hz, Min_pulse, Max_pulse>>();
-            }
+        template<uint8_t Pos = 127>
+        static void init() {
+            llib::target::pin_pwm<pin>::template init<Hz, _to_dutycyle(Pos)>();
+        }
 
-            template<uint8_t Pos>
-            constexpr static void set() {
-                llib::target::pin_pwm<pin>::template set<detail::_dutycycle<Pos, Hz, Min_pulse, Max_pulse>>();
-            }            
-            
-            constexpr static void set(uint8_t Pos) {
-                llib::target::pin_pwm<pin>::set(_to_dutycyle(Pos));
-            }        
+        template<uint8_t Pos>
+        constexpr static void set() {
+            llib::target::pin_pwm<pin>::template set<_to_dutycyle(Pos)>();
+        }
+
+        constexpr static void set(uint8_t Pos) {
+            llib::target::pin_pwm<pin>::set(_to_dutycyle(Pos));
+        }
     };
 }
 
