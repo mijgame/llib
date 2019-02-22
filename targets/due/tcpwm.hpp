f4622c0821b288eb8caf250f7a6a65298634c6ad
itzandroidtab <koendegraaff@itzandroidtab.nl>
2019-02-23 00:48:26 +0100

    added handlers for timers

--- targets/due/tcpwm.hpp
+++ targets/due/tcpwm.hpp
@@ -115,7 +115,7 @@ namespace llib::due{
                         T_ch->TC_RA = (value * freq) / 0xFF;
                         T_ch->TC_CMR = (T_ch->TC_CMR & ~(TC_CMR_ACPA_Msk | TC_CMR_ACPC_Msk)) | TC_CMR_ACPA_CLEAR | TC_CMR_ACPC_SET;
                     } else {
-                        T_ch->TC_CMR = (T_ch->TC_CMR & ~(TC_CMR_ACPA_Msk | TC_CMR_ACPC_Msk)) | TC_CMR_ACPA_CLEAR | TC_CMR_ACPC_CLEAR;
+                         T_ch->TC_CMR = (T_ch->TC_CMR & ~(TC_CMR_ACPA_Msk | TC_CMR_ACPC_Msk)) | TC_CMR_ACPA_CLEAR | TC_CMR_ACPC_CLEAR;
                     }
                 } 
                 else if(std::is_same_v<typename Pin::timer_pin, tc::tiob>){
@@ -155,6 +155,72 @@ namespace llib::due{
                 }
             }
     };
+}
+
+extern "C" {
+void __TC0_Handler(){
+    llib::due::_handle_isr<llib::due::tc::channel_0>(
+        TC0->TC_CHANNEL[0].TC_SR,
+        TC0->TC_CHANNEL[0].TC_IMR
+    );
+}
+
+void __TC1_Handler(){
+    llib::due::_handle_isr<llib::due::tc::channel_1>(
+        TC0->TC_CHANNEL[1].TC_SR,
+        TC0->TC_CHANNEL[1].TC_IMR
+    );
+}
+
+void __TC2_Handler(){
+    llib::due::_handle_isr<llib::due::tc::channel_2>(
+        TC0->TC_CHANNEL[2].TC_SR,
+        TC0->TC_CHANNEL[2].TC_IMR
+    );
+}
+
+void __TC3_Handler(){
+    llib::due::_handle_isr<llib::due::tc::channel_3>(
+        TC1->TC_CHANNEL[0].TC_SR,
+        TC1->TC_CHANNEL[0].TC_IMR
+    );
+}
+
+void __TC4_Handler(){
+    llib::due::_handle_isr<llib::due::tc::channel_4>(
+        TC1->TC_CHANNEL[1].TC_SR,
+        TC1->TC_CHANNEL[1].TC_IMR
+    );
+}
+
+void __TC5_Handler(){
+    llib::due::_handle_isr<llib::due::tc::channel_5>(
+        TC1->TC_CHANNEL[2].TC_SR,
+        TC1->TC_CHANNEL[2].TC_IMR
+    );
+}
+
+void __TC6_Handler(){
+    llib::due::_handle_isr<llib::due::tc::channel_6>(
+        TC2->TC_CHANNEL[0].TC_SR,
+        TC2->TC_CHANNEL[0].TC_IMR
+    );
+}
+
+void __TC7_Handler(){
+    llib::due::_handle_isr<llib::due::tc::channel_7>(
+        TC2->TC_CHANNEL[1].TC_SR,
+        TC2->TC_CHANNEL[1].TC_IMR
+    );
+}
+
+void __TC8_Handler(){
+    llib::due::_handle_isr<llib::due::tc::channel_8>(
+        TC2->TC_CHANNEL[2].TC_SR,
+        TC2->TC_CHANNEL[2].TC_IMR
+    );
+}
+
 }
 
 #endif
