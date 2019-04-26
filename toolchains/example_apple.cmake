6853b43ad688eaa1941070ea82748633b2e65080
itzandroidtab <koen@koon.io>
2019-04-26 23:41:53 +0200

    updated the examples to have the extra arm commands available

--- toolchains/example_apple.cmake
+++ toolchains/example_apple.cmake
@@ -1,8 +1,15 @@
 cmake_minimum_required(VERSION 3.11)
 
-set(OBJCOPY "/opt/arm-none-eabi/bin/arm-none-eabi-objcopy")
+# compiler settings
 set(CMAKE_C_COMPILER "/opt/arm-none-eabi/bin/arm-none-eabi-gcc")
 set(CMAKE_CXX_COMPILER "/opt/arm-none-eabi/bin/arm-none-eabi-g++")
+
+# arm-eabi
+set(ARM_EABI_OBJCOPY "/opt/arm-none-eabi/bin/arm-none-eabi-objcopy")
+set(ARM_EABI_OBJDUMP "/opt/arm-none-eabi/bin/arm-none-eabi-objdump")
+set(ARM_EABI_SIZE "/opt/arm-none-eabi/bin/arm-none-eabi-size") 
+
+# other settings
 set(TERMINAL "${CMAKE_CURRENT_SOURCE_DIR}/tools/lpc21isp_197/lpc21isp_hr_linux")
 set(BOSSAC "bossac")
 set(SERIAL_PORT /dev/ttyACM0)
--- toolchains/example_linux.cmake
+++ toolchains/example_linux.cmake
@@ -1,8 +1,15 @@
 cmake_minimum_required(VERSION 3.11)
 
-set(OBJCOPY "$ENV{HOME}/opt/gcc-arm-none-eabi-7-2018-q2-update/bin/arm-none-eabi-objcopy")
+# compiler settings
 set(CMAKE_C_COMPILER "$ENV{HOME}/opt/gcc-arm-none-eabi-7-2018-q2-update/bin/arm-none-eabi-gcc")
 set(CMAKE_CXX_COMPILER "$ENV{HOME}/opt/gcc-arm-none-eabi-7-2018-q2-update/bin/arm-none-eabi-g++")
+
+# arm-eabi
+set(ARM_EABI_OBJCOPY "$ENV{HOME}/opt/gcc-arm-none-eabi-7-2018-q2-update/bin/arm-none-eabi-objcopy")
+set(ARM_EABI_OBJDUMP "$ENV{HOME}/opt/gcc-arm-none-eabi-7-2018-q2-update/bin/arm-none-eabi-objdump")
+set(ARM_EABI_SIZE "$ENV{HOME}/opt/gcc-arm-none-eabi-7-2018-q2-update/bin/arm-none-eabi-size") 
+
+# other settings
 set(TERMINAL "${CMAKE_CURRENT_SOURCE_DIR}/tools/lpc21isp_197/lpc21isp_hr_linux")
 set(BOSSAC "bossac")
 set(SERIAL_PORT /dev/ttyACM0)
