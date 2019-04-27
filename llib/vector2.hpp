ac0c7d363289cf05f84b37c16676188638ecb897
Lex Ruesink <lexruesink@live.nl>
2019-04-27 12:24:23 +0200

    Rename to vector2 to avoid ambiguity

---
+++ llib/vector2.hpp
@@ -0,0 +1,200 @@
+#ifndef LLIB_VECTOR2_HPP
+#define LLIB_VECTOR2_HPP
+
+#include <math.hpp>
+
+namespace llib {
+    /**
+     * Templated 2-dimensional vector
+     * class.
+     *
+     * @tparam T
+     */
+    template<typename T>
+    struct vector2 {
+        T x;
+        T y;
+
+        /**
+         * Default; initialize to zero.
+         */
+        constexpr vector2() : x(0), y(0) {}
+
+        /**
+         * Initialize the vector with the given
+         * values.
+         *
+         * @param x
+         * @param y
+         */
+        constexpr vector2(T x, T y)
+            : x(x), y(y) {}
+
+        /**
+         * Initialize both values with
+         * the given value.
+         *
+         * @param val
+         */
+        constexpr explicit vector2(T val)
+            : x(val), y(val) {}
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 operator+(const vector2 &other) const {
+            return vector2(x + other.x, y + other.y);
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 &operator+=(const vector2 &other) {
+            x += other.x;
+            y += other.y;
+
+            return *this;
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 operator+(T other) const {
+            return vector2(x + other, y + other);
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 &operator+=(T other) {
+            x += other.x;
+            y += other.y;
+
+            return *this;
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 operator-(const vector2 &other) const {
+            return vector2(x - other.x, y - other.y);
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 &operator-=(const vector2 &other) {
+            x -= other.x;
+            y -= other.y;
+
+            return *this;
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 operator-(T other) const {
+            return vector2(x - other.x, y - other.y);
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 &operator-=(T other) {
+            x -= other.x;
+            y -= other.y;
+
+            return *this;
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 operator*(const vector2 &other) const {
+            return vector2(x * other.x, y * other.y);
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 &operator*=(const vector2 &other) {
+            x *= other.x;
+            y *= other.y;
+
+            return *this;
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 operator*(T other) const {
+            return vector2(x * other.x, y * other.y);
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 &operator*=(T other) {
+            x *= other.x;
+            y *= other.y;
+
+            return *this;
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 operator/(const vector2 &other) const {
+            return vector2(x / other.x, y / other.y);
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 &operator/=(const vector2 &other) {
+            x /= other.x;
+            y /= other.y;
+
+            return *this;
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 operator/(T other) const {
+            return vector2(x / other.x, y / other.y);
+        }
+
+        /**
+         * @param other
+         * @return
+         */
+        constexpr vector2 &operator/=(T other) {
+            x /= other.x;
+            y /= other.y;
+
+            return *this;
+        }
+    };
+
+    // some nice aliases
+    using vector2i = vector2<int_fast32_t>;
+    using vector2u = vector2<uint_fast32_t>;
+}
+
+#endif //LLIB_VECTOR2_HPP
