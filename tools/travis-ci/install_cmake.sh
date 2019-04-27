ef42c8bc7940b34f60e87b1f2a39a7644eac5a6d
Koon <koen@itzandroidtab.nl>
2019-04-27 20:22:25 +0200

    Fix for unbound variable

--- tools/travis-ci/install_cmake.sh
+++ tools/travis-ci/install_cmake.sh
@@ -3,12 +3,12 @@
 set -eu -o pipefail
 
 echo "-- Installing CMake"
-if [[ -f $HOME/cmake/$CMAKE_VERSION/bin/cmake ]]; then
-echo "-- CMake $CMAKE_VERSION FOUND in cache"
+if [[ -f $HOME/cmake/$1/bin/cmake ]]; then
+echo "-- CMake $1 FOUND in cache"
 else
-echo "-- CMake $CMAKE_VERSION NOT FOUND in cache"
-target_path=$HOME/cmake/$CMAKE_VERSION
-cmake_url="https://cmake.org/files/v${CMAKE_VERSION%.*}/cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz"
+echo "-- CMake $1 NOT FOUND in cache"
+target_path=$HOME/cmake/$1
+cmake_url="https://cmake.org/files/v${1%.*}/cmake-${1}-Linux-x86_64.tar.gz"
 mkdir -p "$target_path"
 curl -Ls "$cmake_url" | tar -xz -C "$target_path" --strip-components=1
 fi
