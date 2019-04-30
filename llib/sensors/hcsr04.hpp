b3d151ea17d91061294042f2eb9e8380ed04893e
Lex Ruesink <lexruesink@live.nl>
2019-04-30 21:08:30 +0200

    Also started this. Doesn't work yet

---
+++ llib/sensors/hcsr04.hpp
@@ -0,0 +1,40 @@
+#ifndef LLIB_HCSR04_HPP
+#define LLIB_HCSR04_HPP
+
+#include <wait.hpp>
+#include <peripheral.hpp>
+
+namespace llib::sensors {
+    template<typename TriggerPin, typename EchoPin>
+    class hcsr04 {
+    public:
+        static uint32_t read() {
+            // Clear
+            TriggerPin::template set<false>();
+            llib::wait_for(llib::us{5});
+
+            // Trigger the sensor
+            TriggerPin::template set<true>();
+            llib::wait_for(llib::us{10});
+            TriggerPin::template set<false>();
+
+            const llib::us us{10};
+            uint32_t duration = 0;
+
+            // Wait to go high..
+            while (!EchoPin::get());
+
+            while (EchoPin::get()) {
+                llib::cout << EchoPin::get() << llib::endl;
+                wait_for(us);
+                duration += 1;
+            }
+
+            uint32_t distance = (duration / 2) / 29; // Should be 29.1 ...
+
+            return distance;
+        }
+    };
+}
+
+#endif //LLIB_HCSR04_HPP
