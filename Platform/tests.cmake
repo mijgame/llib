7bd9dfb408fa5794f9c919f51888848802105216
itzandroidtab <koen@koon.io>
2019-09-14 22:29:46 +0200

    added stream_target for the tests

--- Platform/tests.cmake
+++ Platform/tests.cmake
@@ -2,7 +2,9 @@
 set(TARGET_INCLUDE "${CMAKE_CURRENT_SOURCE_DIR}/targets/tests")
 
 # Target specific sources
-set(TARGET_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/targets/tests/wait.cpp")
+set(TARGET_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/targets/tests/wait.cpp"
+                    targets/tests/stream_target.cpp
+)
 
 # Cpu flags for the tests
 set(TARGET_CPU_FLAGS "")
