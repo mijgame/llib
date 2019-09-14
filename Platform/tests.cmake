2fee112ac751eab02d3c33f0209ae16c8039d978
itzandroidtab <koen@koon.io>
2019-09-14 21:15:11 +0200

    added tests cmake file

---
+++ Platform/tests.cmake
@@ -0,0 +1,9 @@
+# Target specific includes
+set(TARGET_INCLUDE "${CMAKE_CURRENT_SOURCE_DIR}/targets/tests")
+
+# Target specific sources
+set(TARGET_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/targets/tests/wait.cpp")
+
+# Cpu flags for the tests
+set(TARGET_CPU_FLAGS "")
+
