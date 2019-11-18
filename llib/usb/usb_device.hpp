239e30b3e33bd8ed2a82794ea4abb6430e7c4d90
itzandroidtab <koen@koon.io>
2019-11-18 21:58:29 +0100

    made a start with the usb devices

---
+++ llib/usb/usb_device.hpp
@@ -0,0 +1,13 @@
+#ifndef LLIB_USB_DEVICE_HPP
+#define LLIB_USB_DEVICE_HPP
+
+namespace llib::usb {
+    enum class usb_speed {
+        low_speed,
+        full_speed,
+        high_speed
+    };
+} // namespace llib::usb::device
+
+
+#endif
