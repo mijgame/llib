fb966b629b04d9636ce5ada1b272c9baf211f5d3
itzandroidtab <koen@koon.io>
2019-12-25 14:19:39 +0100

    removed CMSIS include and changed to correct name in cmake file

--- Platform/mcb2130.cmake
+++ Platform/mcb2130.cmake
@@ -13,10 +13,10 @@ set(TARGET_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/targets/chips/lpc2138/wait.cpp"
 # Vendor specific includes
 set(VENDOR_INCLUDE "${CMAKE_CURRENT_SOURCE_DIR}/vendor/cortex/nxp")
 
-# Linkerscript for Arduino Due
+# Linkerscript for the lpc2138
 set(LINKERSCRIPT_PATH "${CMAKE_CURRENT_SOURCE_DIR}/boot/lpc2138/linkerscript.ld")
 
-# Cpu flags for the due
-set(TARGET_CPU_FLAGS "-Os -mcpu=arm7tdmi-s -march=armv4t") # -mthumb
+# Cpu flags for the lpc2138 (thumb mode is supported)
+set(TARGET_CPU_FLAGS "-Os -mcpu=arm7tdmi-s -march=armv4t -mthumb") #
 
 set(LLIB_TARGET "lpc2138")
--- targets/chips/lpc2138/device_include.hpp
+++ targets/chips/lpc2138/device_include.hpp
@@ -1,2 +1,9 @@
 #include <stdint.h>
-#include <CMSIS/Include/cmsis_gcc.h>
+
+/**
+  \brief   Count leading zeros
+  \details Counts the number of leading zeros of a data value.
+  \param [in]  value  Value to count the leading zeros
+  \return             number of leading zeros in value
+ */
+#define __CLZ             (uint8_t)__builtin_clz
